5d07e1c27a1543d7af215458e3540eed371914ab
Michael Sutton <hicallmesutton@gmail.com>
2021-02-27 14:54:56 -0600

    commit tree 3f4ccd614a1db5b3b545442cf94b61bf49f362c9

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,54 @@
 // Code YouTubeDebugger Component Here
+// Code DigitalClicker Component Here
+import React from 'react'
+
+class YouTubeDebugger extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+       errors: [],
+       user: null,
+       settings: {
+         bitrate: 8,
+         video: {
+           resolution: '1080p'
+         }
+       }
+    }
+  }
+
+
+  changeBitrate = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: 12
+      }
+    })
+  }
+
+  changeResolution = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        video: {
+          ...this.state.video,
+          resolution: '720p'
+        }
+      }
+    })
+  }
+
+  render () {
+     return (
+       <p>
+         <button className='bitrate' onClick={this.changeBitrate}>bitrate</button>
+         <button className='resolution' onClick={this.changeResolution}>resolution</button>
+       </p>
+    )
+  }
+
+}
+
+export default YouTubeDebugger
